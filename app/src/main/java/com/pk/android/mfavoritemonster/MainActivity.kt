c94e33c56969d398e2ca4da7638f419903462516
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-05-20 12:02:45 +1000

    Manage data entry with editText

--- app/src/main/java/com/pk/android/mfavoritemonster/MainActivity.kt
+++ app/src/main/java/com/pk/android/mfavoritemonster/MainActivity.kt
@@ -4,29 +4,24 @@ import android.graphics.Color
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var container: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        container = findViewById(R.id.linearLayout)
-
-        addTextView(getString(R.string.Hello))
-        addTextView(getString(R.string.From))
-        addTextView(getString(R.string.Androiddeveloper))
-    }
-    private  fun addTextView(label: String){
-        val view = TextView(this)
-        view.text = label
-        view.textSize = 28f
-        view.setTextColor(Color.parseColor("#ff0000"))
-        container.addView(view)
+        button.setOnClickListener {
+            val userName = nameInput.text.toString();
+            val password = passwordInput.text.toString();
+            
+            Toast.makeText(this, "Name : $userName, Password: $password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
