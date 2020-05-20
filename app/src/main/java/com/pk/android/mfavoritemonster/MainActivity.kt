cfd568d1056b73ad87fa1aedcfeb00cf82e691ea
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-05-20 12:46:41 +1000

    use string resources with placeholders

--- app/src/main/java/com/pk/android/mfavoritemonster/MainActivity.kt
+++ app/src/main/java/com/pk/android/mfavoritemonster/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pk.android.mfavoritemonster
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.LinearLayout
@@ -12,16 +13,18 @@ import kotlinx.android.synthetic.main.content_main.*
 class MainActivity : AppCompatActivity() {
 
 
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         button.setOnClickListener {
-            val userName = nameInput.text.toString();
-            val password = passwordInput.text.toString();
+            val userName = nameInput.text.toString()
+            val password = passwordInput.text.toString()
+            val message = getString(R.string.login_message, userName,password)
 
-            Toast.makeText(this, "Name : $userName, Password: $password", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
     }
 }
