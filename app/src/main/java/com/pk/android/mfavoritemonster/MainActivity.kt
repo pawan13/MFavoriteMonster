be07cfd9c30d27822336b861cf9a42af4bd152c5
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-05-20 14:23:00 +1000

    Display image resources and load the images at runtime

--- app/src/main/java/com/pk/android/mfavoritemonster/MainActivity.kt
+++ app/src/main/java/com/pk/android/mfavoritemonster/MainActivity.kt
@@ -12,27 +12,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
     @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        button.setOnClickListener {
-            val userName = nameInput.text.toString()
-            val password = passwordInput.text.toString()
-            val message = getString(R.string.login_message, userName,password)
-
-//            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-            Snackbar.make(it, message, Snackbar.LENGTH_LONG)
-                .setAction("Click me", {showAnotherMessage()})
-                .show()
+        button1.setOnClickListener {
+            displayImageResources(R.drawable.monster01)
+        }
+        button2.setOnClickListener {
+            displayImageResources(R.drawable.monster02)
+        }
+        button3.setOnClickListener {
+            displayImageResources(R.drawable.monster03)
         }
     }
 
-    private fun showAnotherMessage() {
-        Toast.makeText(this, "You  clicked", Toast.LENGTH_SHORT).show();
+    private fun displayImageResources(resId: Int) {
+        monsterimage.setImageResource(resId)
     }
 }
